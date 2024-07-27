1aa7437de57f4333756fe3f402608590bb87a110
Muhammad Khairul Anwar <khairul240102@gmail.com>
2024-07-27 13:47:33 +0700

    first commit

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permission = [
+            'view courses',
+            'create courses',
+            'edit courses',
+            'delete courses',
+        ];
+
+        foreach($permission as $permission) {
+            Permission::create([
+                'name' => $permission
+            ]);
+        }
+
+        $teacherRole = Role::create([
+            'name' => 'teacher'
+        ]);
+
+        $teacherRole->givePermissionTo([
+            'view courses',
+            'create courses',
+            'edit courses',
+            'delete courses',
+        ]);
+
+        $studentRole = Role::create([
+            'name' => 'student',
+        ]);
+
+        $studentRole->givePermissionTo([
+            'view courses',
+        ]);
+
+        // membuat data user super admin
+        $user = User::create([
+            'name' => 'Anwar',
+            'email' => 'anwar@teacher.com',
+            'password' => bcrypt('123123123'),
+        ]);
+
+        $user->assignRole($teacherRole);
+    }
+
+
+}
