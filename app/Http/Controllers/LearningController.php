b8755a0da27375a0867b24007328055be6cfe8df
Muhammad Khairul Anwar <khairul240102@gmail.com>
2024-07-29 06:36:58 +0700

    routing role student

---
+++ app/Http/Controllers/LearningController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LearningController extends Controller
+{
+    //
+
+    public function index() {
+        return 'Hello Student, welcome and lets study';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\LearningController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +17,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('dashboard')->name('dashboard.')->group(function() {
+        Route::resource('courses', CourseController::class)->middleware('role:teacher');
+    });
+
+    Route::get('/learning', [LearningController::class, 'index'])->middleware('role:student')->name('learning.index');
 });
 
 require __DIR__.'/auth.php';
